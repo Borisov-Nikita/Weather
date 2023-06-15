d443fbae10f20afb9e8d6aac7127ae1dc3cbc1dc
Borisov Nikita <borisov-ta@yandex.ru>
2023-06-15 14:45:58 +0400

    Created project

---
+++ app/src/main/java/nik/borisov/weather/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package nik.borisov.weather.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nik.borisov.weather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
